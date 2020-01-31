8683f62933f851c64663b42e47ed8f7a9efff7fe
Nekiedra Singleton <nekiedrasingleton@gmail.com>
2020-01-30 19:22:42 -0600

    added cart to screen

--- app.js
+++ app.js
@@ -1,43 +1,77 @@
 let cart = [];
+let currencyRate = 1;
+let currencySymbol = '&dollar;';
 let items = document.querySelectorAll('.item');
 for (let i = 0; i < items.length; i++) {
     const item = items[i];
-    console.log(item);
-    
-    item.addEventListener('click', function() {
-        console.log(item.innerText);
-        let found = false;
-        let total = 0;
-        let price = item.getAttribute('data-price');
-        for (let j = 0; j< cart.length; j++) {
-            const cartItem = cart[j];
+    item.addEventListener('click', function () {
+        addToCart(item);
+    });
+}
+let changeCurrency = document.getElementById('currency');
+changeCurrency.addEventListener('change', updateCurrencyRate);
+let viewCart = document.getElementById('viewCart');
+viewCart.addEventListener('click', function(){
+    showCart()
+})
 
-            if (cartItem.name == item.innerText) {
-                found = true;
-                cart[j].qty++;
-                
-            }
-            
+function addToCart(item) {
+    let found = false;
+    let price = item.getAttribute('data-price');
+    let name = item.innerText;
+    for (let j = 0; j < cart.length; j++) {
+        const cartItem = cart[j];
+        if (cartItem.name == name) {
+            found = true;
+            cart[j].qty++;
         }
-        if(!found){
-            cart.push({name: item.innerText, price ,qty:1});
-        }
-        function updateItems(){
-            let numberItems = document.getElementById('numberItems');
-            numberItems.innerText = cart.length;
-        };
+    }
+    if (!found) {
+        cart.push({name, price, qty: 1});
+    }
+    updateItems();
+    updateTotal();    
+}
+function updateCurrencyRate() {
+    let country = this.value;
+    switch (country) {
+        case 'us':
+            currencyRate = 1;
+            currencySymbol = '&dollar;';
+            break;
+        case 'uk':
+            currencyRate = 1.5;
+            currencySymbol = '&pound;';
+            break;
+        default:
+            break;
+    }
+    updateTotal();
+}
+function updateItems() {
+    let numberItems = document.getElementById('numberItems');
+    numberItems.innerText = cart.length;
+}
+function updateTotal() {
+    let cartTotal = document.getElementById('cartTotal');
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const item = cart[i];
+        total += item.price * item.qty;
+    }
 
-        function updateTotal(){
-            let cartTotal = document.getElementById('cartTotal');
-            for (let i = 0; i < cart.length; i++) {
-                const item = cart[i];
-                total += item.price * item.qty;
-                
-            }
-            cartTotal.innerText = total.toFixed(2);
-        }
-        updateItems();
-        updateTotal();
-    })
-    
+    total = total * 1.07;
+    total /= currencyRate;
+    cartTotal.innerHTML =   `${currencySymbol}${total.toFixed(2)}`;
+}
+function showCart(){
+    let cartList = document.getElementById('cart');
+    cartList.innerText = '';
+    for (let i = 0; i < cart.length; i++) {
+        const item = cart[i];
+        let el = document.createElement('li');
+        el.innerText = `${item.name} @${item.price} QTY: ${item.qty}`;
+        cartList.appendChild(el);
+        
+    }
 }
